ec5b36d2afc391afb97ef74543087521e6ccad58
Ron V <ronneyvargas@gmail.com>
2019-12-23 14:18:43 -0600

    added scrolling function and navbar hides when nav-link is clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // (function ($) {
 //     'use strict';
 
@@ -19,23 +20,27 @@
 
 // })(jQuery);
 
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//   var topoffset = 122;
+  var topoffset = 122;
 
-//   // Use smooth scrolling when clicking on navigation
-//   $('.navbar a[href*=\\#]:not([href=\\#])').click(function () {
-//     if (location.pathname.replace(/^\//, '') ===
-//       this.pathname.replace(/^\//, '') &&
-//       location.hostname === this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top - topoffset + 2
-//         }, 500);
-//         return false;
-//       } //target.length
-//     } //click function
-//   }); //smooth scrolling
-// })
+  // Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=\\#]:not([href=\\#])').click(function () {
+    if (location.pathname.replace(/^\//, '') ===
+      this.pathname.replace(/^\//, '') &&
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - topoffset + 2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+  $('.navbar-nav>li>a').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+})
