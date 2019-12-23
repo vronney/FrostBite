1bcc8869af0e3e35a4f24715c879a46fa810d9f1
Ron V <ronneyvargas@gmail.com>
2019-12-22 22:19:13 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// (function ($) {
+//     'use strict';
+
+// $(window).load(function() {
+//     $('#preloader-wrapper').fadeOut('slow');
+// });
+
+//     $('.gallery-slider').owlCarousel(
+//         {
+//         pagination : true,
+//         autoPlay : 5000,
+//         itemsDesktop  :  [1500,4],
+//         itemsDesktopSmall :  [979,3]
+//         }
+//     );
+
+//     // Gallery Popup
+//     $('.image-popup').magnificPopup({type:'image'});
+
+// })(jQuery);
+
+// $(document).ready(function () {
+
+//   var topoffset = 122;
+
+//   // Use smooth scrolling when clicking on navigation
+//   $('.navbar a[href*=\\#]:not([href=\\#])').click(function () {
+//     if (location.pathname.replace(/^\//, '') ===
+//       this.pathname.replace(/^\//, '') &&
+//       location.hostname === this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top - topoffset + 2
+//         }, 500);
+//         return false;
+//       } //target.length
+//     } //click function
+//   }); //smooth scrolling
+// })
